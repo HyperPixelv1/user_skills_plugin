a8c9ce0e106a2a1f45bc8faf1ee825ef010d94ae
Furkan Yağız Kurşunlu <79865741+HyperPixelv1@users.noreply.github.com>
2024-10-09 14:28:26 +0300

    Delete user_skills_plugin directory

--- user_skills_plugin/app/models/user_skill.rb
+++
@@ -1,6 +0,0 @@
-class UserSkill < ActiveRecord::Base
-  belongs_to :user
-  has_many :user_skills
-  validates :tag_name, presence: true
-  validates :task_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-end
--- user_skills_plugin/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :issues, only: [:index, :show] do
-    member do
-      post "update_skills", to: "user_skills#update_skills"
-    end
-  end
-end
