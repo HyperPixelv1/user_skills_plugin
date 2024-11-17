fb38dbfddf1cdf0c8ba402ee62b5d5c1073fe907
Furkan Yağız Kurşunlu <79865741+HyperPixelv1@users.noreply.github.com>
2024-11-17 23:42:11 +0300

    Delete lib directory

--- lib/user_skills_plugin/hooks/user_profile_hook.rb
+++
@@ -1,7 +0,0 @@
-module UserSkillsPlugin
-  module Hooks
-    class UserProfileHook < Redmine::Hook::ViewListener
-      render_on :view_users_show_bottom, partial: 'users/user_skills'
-    end
-  end
-end
