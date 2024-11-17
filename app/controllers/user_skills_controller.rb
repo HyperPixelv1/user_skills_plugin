99d6c264d8cf0a608c05f2f32366ce0887cbf7f9
Furkan Yağız Kurşunlu <79865741+HyperPixelv1@users.noreply.github.com>
2024-11-17 23:36:30 +0300

    Add files via upload

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,54 +1,19 @@
 class UserSkillsController < ApplicationController
-  before_action :find_issue, only: [:update_skills]
-  before_action :authorize_only, only: [:tagged_issue]
-  before_action :require_login, only: [:tagged_issue]
-  before_action :set_cache_headers, only: [:tagged_issue]
-
-  def update_skills(issue)
-    return unless issue && (issue.status.name.casecmp("done").zero? && issue.spent_hours >= 0.5)
-
-    tags = issue.tag_list.to_a  # Burada tag_list'i diziye çeviriyoruz
-    user = issue.assigned_to
-    return unless user
-
-    tags.each do |tag|
-      user_skill = UserSkill.find_or_initialize_by(user_id: user.id, tag_name: tag.strip)
-      user_skill.task_count = user_skill.task_count.to_i + 1
-      if user_skill.save
-        Rails.logger.info "Skill saved for User ID: #{user.id}, Tag: #{tag}, Task Count: #{user_skill.task_count}"
-      else
-        Rails.logger.error "Failed to save skill for User ID: #{user.id}, Tag: #{tag}, Errors: #{user_skill.errors.full_messages.join(", ")}"
-      end
-    end
-    Rails.logger.info "Updated skills for User ID: #{user.id}, Tags: #{tags.join(", ")}"
-  end
+  unloadable
 
   def index
-    @user = User.find(params[:id])
-    @user_skills = UserSkill.where(user: @user)
-
-    if @user_skills.any?
-      Rails.logger.info "Displaying skills for User ID: #{@user.id}, Skills Count: #{@user_skills.count}"
-    else
-      Rails.logger.info "No skills available for User ID: #{@user.id}."
-    end
-  end
-
-  private
-
-  def find_user
     @user = User.find(params[:user_id])
-  rescue ActiveRecord::RecordNotFound => e
-    Rails.logger.error "User not found: #{e.message}"
-    redirect_to users_path, alert: "User not found." # Kullanıcı bulunamazsa yönlendirme yap
-  end
 
-  private
+    # Etiketleri dinamik olarak çekiyoruz
+    skills_query = AdditionalTag.joins("JOIN additional_taggings ON additional_taggings.tag_id = additional_tags.id")
+                                .joins("JOIN (SELECT issue_id FROM time_entries WHERE user_id = #{@user.id} GROUP BY issue_id HAVING SUM(hours) > 1) AS te ON te.issue_id = additional_taggings.taggable_id")
+                                .where("additional_taggings.taggable_type = ?", "Issue")
+                                .group("additional_tags.name")
+                                .count
 
-  def find_issue
-    @issue = Issue.find_by(id: params[:issue_id])
-    unless @issue
-      redirect_to root_path, alert: "Issue not found."
+    # Görünüme uygun formatlama
+    @skills = skills_query.map do |tag_name, task_count|
+      { tag_name: tag_name, task_count: task_count }
     end
   end
 end
