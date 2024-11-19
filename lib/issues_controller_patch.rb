a9521f6759ad1184fb7b984f4eae4e50cc78ba2e
HyperPixelWSL <furkankursunlu@hotmail.com>
2024-11-19 09:23:10 +0300

    Revert changes from 0276dc2772ee115f15d8a5c28fec78dba4028ff9 to HEAD

---
+++ lib/issues_controller_patch.rb
@@ -0,0 +1,21 @@
+require_dependency "issues_controller"
+
+module IssuesControllerPatch
+  def self.included(base)
+    base.class_eval do
+      after_action :call_user_skills_update, only: [:update]
+    end
+  end
+
+  private
+
+  def call_user_skills_update
+    return unless @issue.saved_change_to_status_id? && (@issue.status.name&.casecmp("done")&.zero? && @issue.spent_hours >= 0.5)
+
+    # Kullanıcı yeteneklerini güncelleyen controller'ı çağır
+    UserSkillsController.new.update_skills(@issue)
+  end
+end
+
+# Patch'i uygulamak
+IssuesController.send(:include, IssuesControllerPatch)
--- lib/user_skills_plugin/hooks/user_profile_hook.rb
+++ lib/user_skills_plugin/hooks/user_profile_hook.rb
@@ -1,7 +1,15 @@
 module UserSkillsPlugin
   module Hooks
     class UserProfileHook < Redmine::Hook::ViewListener
-      render_on :view_users_show_bottom, partial: 'users/user_skills'
+      def view_account_left_bottom(context = {})
+        user = context[:user]
+        skills = UserSkill.where(user: user)
+
+        context[:controller].send(:render_to_string, {
+          partial: "users/user_skills",
+          locals: { skills: skills, user: user },
+        })
+      end
     end
   end
 end
